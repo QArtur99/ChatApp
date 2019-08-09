f25e5c454aacb95cee94e570c52b7f57c7162bba
ART_F <artur2113@o2.pl>
2019-08-09 23:06:19 +0200

    refactor clean login module

--- app/src/main/java/com/artf/chatapp/App.kt
+++ app/src/main/java/com/artf/chatapp/App.kt
@@ -1,29 +1,12 @@
 package com.artf.chatapp
 
 import android.app.Application
-import com.artf.chatapp.di.AppComponent
-import com.artf.chatapp.di.AppModule
-import com.artf.chatapp.di.DaggerAppComponent
-import com.artf.chatapp.di.FirebaseRepositoryModule
 import com.google.firebase.database.FirebaseDatabase
 
-
 class App : Application() {
-
-    companion object {
-        lateinit var app: App
-        val component: AppComponent by lazy {
-            DaggerAppComponent.builder()
-                .appModule(AppModule(app))
-                .firebaseRepositoryModule(FirebaseRepositoryModule())
-                .build()
-        }
-    }
 
     override fun onCreate() {
         super.onCreate()
         FirebaseDatabase.getInstance().setPersistenceEnabled(true)
-        App.app = this
-        component.inject(this)
     }
 }
